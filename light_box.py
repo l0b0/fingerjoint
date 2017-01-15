d27926c6f6a163e6725ff76934e1804a472597bf
Victor Engmark <victor.engmark@gmail.com>
2017-01-15 12:43:30 +0000

    Format secondary file for PEP8 compliance

--- light_box.py
+++ light_box.py
@@ -6,17 +6,19 @@ KERF = 0.2
 
 
 def main():
-	width = 150
-	height = 150
-	depth = 75
-	top_panel_FJM = FingerJointMaker(width, height, MATERIAL_WIDTH, (MATERIAL_WIDTH * (1.0 + MATERIAL_VARIANCE)), (2,2,2,2), KERF)
-	top_panel_FJM.make()
-	top_panel_FJM.svg(filename='top.svg')
+    width = 150
+    height = 150
+    depth = 75
+    top_panel_finger_joint_maker = FingerJointMaker(
+        width, height, MATERIAL_WIDTH, (MATERIAL_WIDTH * (1.0 + MATERIAL_VARIANCE)), (2, 2, 2, 2), KERF)
+    top_panel_finger_joint_maker.make()
+    top_panel_finger_joint_maker.svg(filename='top.svg')
 
-	side_panel_FJM = FingerJointMaker(width, depth, MATERIAL_WIDTH, (MATERIAL_WIDTH * (1.0 + MATERIAL_VARIANCE)), (5,3,5,5), KERF)
-	side_panel_FJM.make()
-	side_panel_FJM.svg(filename='side.svg')
+    side_panel_finger_joint_maker = FingerJointMaker(
+        width, depth, MATERIAL_WIDTH, (MATERIAL_WIDTH * (1.0 + MATERIAL_VARIANCE)), (5, 3, 5, 5), KERF)
+    side_panel_finger_joint_maker.make()
+    side_panel_finger_joint_maker.svg(filename='side.svg')
 
 
 if __name__ == '__main__':
-	main()
+    main()
